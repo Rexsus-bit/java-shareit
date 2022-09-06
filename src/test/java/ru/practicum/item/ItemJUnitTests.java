56dadf219ce611c372b5611771896c067919b409
romanbizin <roman.bizin2016@yandex.ru>
2022-09-06 18:55:56 +0300

    added tests

--- src/test/java/ru/practicum/item/ItemJUnitTests.java
+++ src/test/java/ru/practicum/item/ItemJUnitTests.java
@@ -1,14 +1,23 @@
 package ru.practicum.item;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.OngoingStubbing;
 import ru.practicum.booking.BookingJpaRepository;
 import ru.practicum.common.Mapper;
+import ru.practicum.exceptions.NotExistedUserException;
 import ru.practicum.user.User;
 import ru.practicum.user.UserDTO;
 import ru.practicum.user.UserJpaRepository;
+
+import java.util.Collection;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ItemJUnitTests {
@@ -21,13 +30,6 @@ public class ItemJUnitTests {
     private BookingJpaRepository bookingRepository;
     @Mock
     Mapper mapper;
-//    @Mock
-//    private CommentRepository commentRepository;
-//    @Mock
-//    private CommentDtoMapper commentDtoMapper;
-//    @Mock
-//    private BookingDtoMapper bookingDtoMapper;
-
 
     User user = new User(1L, "James", "james@yandex.com");
     UserDTO userDTO = new UserDTO(1L, "James", "james@yandex.com");
@@ -55,6 +57,8 @@ public class ItemJUnitTests {
 //            itemService.getAllUserItems(1, 0, 5);
 //        });
 //    }
+
+
 
 
 }
